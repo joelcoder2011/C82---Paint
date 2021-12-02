f9f25652d482a3c222f5a4447a38557a2dadfbd0
joelcoder2011 <80383585+joelcoder2011@users.noreply.github.com>
2021-12-02 13:45:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,24 @@
-var mouseEvent = "empty";
+ 
+canvas = document.getElementById("myCanvas");
+ color = "red";
+ canvas.getContext("2d").beginPath();
+ canvas.getContext("2d").strokeStyle = color;
+ canvas.getContext("2d").lineWidth = 2;
+ canvas.getContext("2d").arc(200,200,40,0,2*Math.PI);
+ canvas.getContext("2d").stroke();
 
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d")
+ canvas = document.getElementById("myCanvas");
+ color = "orange";
+ canvas.getContext("2d").beginPath();
+ canvas.getContext("2d").strokeStyle = color;
+ canvas.getContext("2d").lineWidth = 2;
+ canvas.getContext("2d").arc(400,200,40,0,4*Math.PI);
+ canvas.getContext("2d").stroke();
 
-color = "red";
-width_of_line = 2;
-
-canvas.addEventListener("mousedown",my_mousedown);
-
-function my_mousedown(e) {
-     color = document.getElementById("color").value;
-     width_of_line = document.getElementById("Width_of_line").value;
-     radius = document.getElementById("radius").value;
-     mouseEvent = "mousedown";
-}
-
-canvas.addEventListener("mousemove",my_mousemove);
-function my_mousemove(e){
-current_postion_of_mouse_x = e.clientX - canvas.offsetLeft;
-current_postion_of_mouse_y = e.clientY - canvas.offsetTop;
-if (mouseEvent == "mousedown")
-{
-    console.log("Current postion of x and y coordinates = ");
-    console.log("x = " + current_postion_of_mouse_x +",y" - "" + current_postion_of_mouse_y);
-    ctx.beginPath();
-    ctx.strokestyle = color;
-    ctx.linewidth = width_of_line;
-    ctx.arc(current_postion_of_mouse_x , current_postion_of_mouse_y , radius , 0 ,2 * Math.PI);
-    ctx.stroke();
-}}
-canvas.addEventListener("mouseup" , my_mouseup);
-function my_mouseup(e)
-{
-      mouseEvent = "mouseup";
-}
-canvas.addEventListener("mouseleave", my_mouseleave);
-function my_mouseleave(e)
-{
-      mouseEvent = "mouseleave";
-}
-function clearArea(e) 
-{
-   ctx.clearArea(0,0,ctx.canvas.width,ctx.canvas.height);
-}
+ canvas = document.getElementById("myCanvas");
+ color = "yellow";
+ canvas.getContext("2d").beginPath();
+ canvas.getContext("2d").strokeStyle = color;
+ canvas.getContext("2d").lineWidth = 2;
+ canvas.getContext("2d").arc(600,200,40,0,4*Math.PI);
+ canvas.getContext("2d").stroke();
